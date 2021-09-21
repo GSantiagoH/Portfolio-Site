3e9b04b92816a78de7d6185996a066a9cf6fb569
GSantiagoH <giovanni.haysel@gmail.com>
2021-09-20 20:50:58 -0400

    Fixed compile issues

--- src/data.js
+++ src/data.js
@@ -1,34 +1,36 @@
+export { testimonials } from "./data";
+export { skills } from "./data";
 export const projects = [
-    {
-        title: "React Reserve",
-        subtitle: "MERN Stack",
-        description:
-        "Placeholder text as an example to be edited out later on.",
-        image: "./project-1.gif",
-        link: "https://reactbootcamp.com",
-    },
-    {
-        title: "React Tracks",
-        subtitle: "React and Python",
-        description:
-        "Placeholder text as an example to be edited out later on.",
-        image: "./project-2.gif",
-        link: "https://reedbarger.com",
-    },
-    {
-        title: "DevChat",
-        subtitle: "React and Firebase",
-        description:
-          "Placeholder text as an example to be edited out later on",
-        image: "./project-3.gif",
-        link: "https://jsbootcamp.com",
-      },
-      {
-        title: "Epic Todo App",
-        subtitle: "React Hooks",
-        description:
-          "Placeholder text as an example to be edited out later on",
-        image: "./project-4.gif",
-        link: "https://pythonbootcamp.com",
-      },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
 ];
